0809183a41f79ff606d204736dfccd7fde1a71f1
bram6134 <77304942+bram6134@users.noreply.github.com>
2022-10-22 13:40:25 -0500

    Update file_system_events_tracker.py

--- file_system_events_tracker.py
+++ file_system_events_tracker.py
@@ -14,7 +14,12 @@ class FileSystemEventHandler(FileSystemEventHandler):
 
     def on_deleted(self, event):
         print(f"Oops! Someone deleted {event.src_path}")
-
+        
+    def on_modified(self, event):
+        print(f"{event.src_path} has been modified")
+        
+    def on_moved(self, event):
+        print(f"{event.src_path} has been moved somewhere else")
 event_handler = FileSystemEventHandler()
 
 observer = Observer()
